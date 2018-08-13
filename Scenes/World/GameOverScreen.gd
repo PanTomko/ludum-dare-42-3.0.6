d4310b3c4326a023a1d2647182281c00667cdbce
PanTomko <pantomko@gmail.com>
2018-08-13 15:13:05 +0200

    export alpha needs testing
    title

--- Scenes/World/GameOverScreen.gd
+++ Scenes/World/GameOverScreen.gd
@@ -1,18 +1,8 @@
 extends CenterContainer
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
 func _input(event):
 	if event is InputEventKey and event.pressed && !($AnimationPlayer.is_playing()):
 		get_parent().bring_menu( 0 )
